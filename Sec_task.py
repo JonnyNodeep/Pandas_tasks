65290e726fe01c68ab1e256a17d2bdfd40aa16da
Jon X <89638552102es@gamil.com>
2025-04-08 01:43:47 +0500

    Удалены столбцы согласно заданию,оставлены только Longitude, Latitude, HouseAge

---
+++ Sec_task.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from sklearn.datasets import fetch_california_housing
+
+# Снова загружаем данные из sklearn
+california = fetch_california_housing()
+df = pd.DataFrame(california.data, columns=california.feature_names)
+df['median_house_value'] = california.target
+
+# Оставляем только нужные столбцы: Longitude, Latitude, HouseAge
+df_trimmed = df[['Longitude', 'Latitude', 'HouseAge']]
+
+# Выводим первые 10 строк результата
+print("Таблица после удаления лишних столбцов:")
+print(df_trimmed.head(10))
+g
+# Выводим информацию о размере таблицы
+print("\nРазмер таблицы:")
+print(f"Количество строк и столбцов: {df_trimmed.shape}")
